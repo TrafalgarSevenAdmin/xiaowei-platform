b7c8cdb0710767b4a307f6bd09a515c9bb02774c
yuanwei <563668429@qq.com>
2018-07-02 16:47:01 +0800

    xxxx

--- src/main/java/com/xiaowei/XiaoweiPlatformApplication.java
+++ src/main/java/com/xiaowei/XiaoweiPlatformApplication.java
@@ -1,4 +1,4 @@
-package com.xiaowei.com.xiaoweiplatform;
+package com.xiaowei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
