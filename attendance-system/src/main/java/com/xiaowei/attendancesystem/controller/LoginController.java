21bf303dde8e8b4f92fbf7716914542973eec786
yuanxuan <563668429@qq.com>
2018-07-19 10:20:16 +0800

    登录

---
+++ attendance-system/src/main/java/com/xiaowei/attendancesystem/controller/LoginController.java
@@ -0,0 +1,66 @@
+package com.xiaowei.attendancesystem.controller;
+
+import com.xiaowei.account.service.ISysUserService;
+import com.xiaowei.account.utils.AccountUtils;
+import com.xiaowei.accountcommon.LoginUserUtils;
+import com.xiaowei.attendancesystem.dto.LoginSysUserDTO;
+import com.xiaowei.core.result.Result;
+import com.xiaowei.core.validate.AutoErrorHandler;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Api(tags = "登录接口")
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+
+    @Autowired
+    private ISysUserService sysUserService;
+
+    /**
+     * 用户登录
+     * @param loginSysUserDTO
+     * @return
+     */
+    @ApiOperation("登录")
+    @PostMapping("/login")
+    @AutoErrorHandler
+    public Result login(@RequestBody @Validated LoginSysUserDTO loginSysUserDTO, BindingResult bindingResult){
+        Subject subject = SecurityUtils.getSubject();
+        subject.login(new UsernamePasswordToken(loginSysUserDTO.getLoginName(),loginSysUserDTO.getPassword()));
+        AccountUtils.loadUser();
+        return Result.getSuccess(LoginUserUtils.getLoginUser());
+    }
+
+
+    /**
+     * 退出登录
+     * @param
+     * @return
+     */
+    @ApiOperation("退出登录")
+    @GetMapping("/logout")
+    public Result logout(){
+        SecurityUtils.getSubject().logout();
+        return Result.getSuccess();
+    }
+
+    /**
+     * 获取当前登录用户的信息
+     * @param
+     * @return
+     */
+    @ApiOperation("获取当前登录用户的信息")
+    @GetMapping("/userInfo")
+    public Result userInfo(){
+        return Result.getSuccess(LoginUserUtils.getLoginUser());
+    }
+
+}
