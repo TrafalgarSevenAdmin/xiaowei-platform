53f008f3dd639d71e82180f78775d3ed4c94620d
yuanxuan <563668429@qq.com>
2018-08-01 11:14:14 +0800

    微信消息推送更新bug

---
+++ mq-core/src/main/java/com/xiaowei/mq/utils/MessageColor.java
@@ -0,0 +1,7 @@
+package com.xiaowei.mq.utils;
+
+public class MessageColor {
+    public static final String SUCCESS = "#00FF00";
+    public static final String ERROR = "#FF0000";
+    public static final String WARNING = "#FFFF00";
+}
