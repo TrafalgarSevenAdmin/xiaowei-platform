50334f789c92d4dcd106abc03b4e09b8b2ab4503
yuanxuan <563668429@qq.com>
2018-08-01 17:07:23 +0800

    字典管理修改各种高级查询条件

--- common-dict/src/main/java/com/xiaowei/commondict/query/DictionaryQuery.java
+++ common-dict/src/main/java/com/xiaowei/commondict/query/DictionaryQuery.java
@@ -1,6 +1,9 @@
 package com.xiaowei.commondict.query;
 
+import com.xiaowei.core.query.rundi.query.Filter;
 import com.xiaowei.core.query.rundi.query.Query;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author mocker
@@ -8,12 +11,36 @@ import com.xiaowei.core.query.rundi.query.Query;
  * @Description 系统权限
  * @Version 1.0
  */
+@Data
 public class DictionaryQuery extends Query {
+    private String code;
+    private String ownCode;
+    private String codeLike;
+    private String name;
+    private String parentId;
+    private Integer level;
 
 
     @Override
     public void generateCondition() {
-
+        if (StringUtils.isNotEmpty(code)) {
+            this.addFilter(Filter.eq("code", code));
+        }
+        if (StringUtils.isNotEmpty(ownCode)) {
+            this.addFilter(Filter.eq("ownCode", ownCode));
+        }
+        if (StringUtils.isNotEmpty(name)) {
+            this.addFilter(Filter.eq("name", name));
+        }
+        if (StringUtils.isNotEmpty(codeLike)) {
+            this.addFilter(Filter.like("code", codeLike + "_%"));
+        }
+        if (StringUtils.isNotEmpty(parentId)) {
+            this.addFilter(Filter.eq("parentId", parentId));
+        }
+        if (level != null) {
+            this.addFilter(Filter.eq("level", level));
+        }
     }
 
 }
