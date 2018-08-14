3c5c7acbaa8577ee05a502e5ed305e634f527c77
yuanxuan <563668429@qq.com>
2018-08-14 13:52:14 +0800

    订单支付完成

--- work-system/src/main/java/com/xiaowei/worksystem/receiver/OrderPayedReceiver.java
+++ work-system/src/main/java/com/xiaowei/worksystem/receiver/OrderPayedReceiver.java
@@ -1,13 +1,20 @@
 package com.xiaowei.worksystem.receiver;
 
+import com.xiaowei.core.utils.EmptyUtils;
 import com.xiaowei.mq.constant.MqQueueConstant;
-import com.xiaowei.worksystem.service.IServiceItemService;
+import com.xiaowei.pay.consts.PayStatus;
+import com.xiaowei.pay.entity.XwOrder;
+import com.xiaowei.pay.repository.XwOrderRepository;
+import com.xiaowei.pay.status.XwType;
 import com.xiaowei.worksystem.service.IWorkOrderService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 /**
  * 订单支付完成
@@ -17,13 +24,31 @@ import org.springframework.stereotype.Component;
 @RabbitListener(queues = MqQueueConstant.ORDER_DEFAULT_PAYED_QUEUE)
 public class OrderPayedReceiver {
 
+    @Autowired
+    private IWorkOrderService workOrderService;
+    @Autowired
+    private XwOrderRepository xwOrderRepository;
+
 
     /**
      * 订单支付完成
      */
     @RabbitHandler
+    @Transactional
     public void messageReceiver(String order) {
         log.info("收到订单" + order + "支付完成通知");
+        Optional<XwOrder> optional = xwOrderRepository.findById(order);
+        EmptyUtils.assertOptional(optional,"没有查询到支付订单");
+        XwOrder xwOrder = optional.get();
+        //判断是否支付完成
+        if(PayStatus.paid.equals(xwOrder.getStatus())){
+            log.info("订单未支付完成:"+order);
+            return;
+        }
+        Integer xwType = xwOrder.getXwType();
+        if(XwType.WORKORDER.getStatus().equals(xwType)){//工单支付订单
+            workOrderService.payServiceItem(xwOrder.getBusinessId());
+        }
 
     }
 
