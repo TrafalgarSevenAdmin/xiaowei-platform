12ca92d67f80b683d551e7db36c059f4e7802af9
yuanxuan <563668429@qq.com>
2018-08-16 14:23:58 +0800

    费用报销标准新增查询条件

--- expense-reimbursement-web/src/main/java/com/xiaowei/expensereimbursementweb/query/ReimbursementStandardQuery.java
+++ expense-reimbursement-web/src/main/java/com/xiaowei/expensereimbursementweb/query/ReimbursementStandardQuery.java
@@ -1,13 +1,39 @@
 package com.xiaowei.expensereimbursementweb.query;
 
+import com.xiaowei.core.query.rundi.query.Filter;
 import com.xiaowei.core.query.rundi.query.Query;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 @Data
 public class ReimbursementStandardQuery extends Query {
+    private String subjectCode;
+    private String shipLevel;
+    private String cityLevel;
+    private String postLevel;
+    private String startCity;
+    private String endCity;
+
 
     @Override
     public void generateCondition() {
-
+        if (StringUtils.isNotEmpty(subjectCode)) {
+            addFilter(new Filter("subjectCode", Filter.Operator.eq, subjectCode));
+        }
+        if (StringUtils.isNotEmpty(shipLevel)) {
+            addFilter(new Filter("shipLevel", Filter.Operator.eq, shipLevel));
+        }
+        if (StringUtils.isNotEmpty(cityLevel)) {
+            addFilter(new Filter("cityLevel", Filter.Operator.eq, cityLevel));
+        }
+        if (StringUtils.isNotEmpty(postLevel)) {
+            addFilter(new Filter("postLevel", Filter.Operator.eq, postLevel));
+        }
+        if (StringUtils.isNotEmpty(startCity)) {
+            addFilter(new Filter("startCity", Filter.Operator.eq, startCity));
+        }
+        if (StringUtils.isNotEmpty(endCity)) {
+            addFilter(new Filter("endCity", Filter.Operator.eq, endCity));
+        }
     }
 }
