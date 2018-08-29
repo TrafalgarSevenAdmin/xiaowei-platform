12dcf9f7d5692e1e350538bdccaae3da50e48e84
yuanxuan <563668429@qq.com>
2018-08-29 13:59:50 +0800

    bug

--- expense-reimbursement/src/main/java/com/xiaowei/expensereimbursement/service/impl/ExpenseFormServiceImpl.java
+++ expense-reimbursement/src/main/java/com/xiaowei/expensereimbursement/service/impl/ExpenseFormServiceImpl.java
@@ -98,7 +98,7 @@ public class ExpenseFormServiceImpl extends BaseServiceImpl<ExpenseForm> impleme
         final WorkOrderSelect workOrderSelect = workOrderSelectRepository.findByCode(workOrderCode);
         EmptyUtils.assertObject(workOrderSelect, "没有查询到所属工单");
         //如果工单已归档,则抛出异常
-        if (workOrderSelect.getSystemStatus() != 10) {
+        if (workOrderSelect.getSystemStatus() == 10) {
             throw new BusinessException("该工单已经关闭!");
         }
         if (workOrderSelect.getSystemStatus() != status) {
