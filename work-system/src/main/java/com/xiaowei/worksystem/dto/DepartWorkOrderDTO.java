ac27e30ce7950d39bcfee8c7eabfaaead47bb7d0
yuanxuan <563668429@qq.com>
2018-08-31 10:12:00 +0800

    bug

--- work-system/src/main/java/com/xiaowei/worksystem/dto/DepartWorkOrderDTO.java
+++ work-system/src/main/java/com/xiaowei/worksystem/dto/DepartWorkOrderDTO.java
@@ -4,7 +4,6 @@ import com.xiaowei.core.validate.V;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -13,7 +12,6 @@ public class DepartWorkOrderDTO {
     private String arriveFileStore;
 
     @ApiModelProperty(value = "到达地点wkt")
-    @NotBlank(groups = {V.Insert.class},message = "到达地点wkt必填!")
     private String wkt;
 
     @ApiModelProperty(value = "到达状态")
