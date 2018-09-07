de8f7ab176c94df6b694bf181d893249b98cf1e1
yuanxuan <563668429@qq.com>
2018-09-07 10:32:21 +0800

    bug

--- account/src/main/java/com/xiaowei/account/query/PostQuery.java
+++ account/src/main/java/com/xiaowei/account/query/PostQuery.java
@@ -1,8 +1,8 @@
 package com.xiaowei.account.query;
 
-import com.xiaowei.account.consts.PostStatus;
 import com.xiaowei.core.query.rundi.query.Filter;
 import com.xiaowei.core.query.rundi.query.Query;
+import com.xiaowei.core.query.rundi.query.Sort;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 
@@ -10,11 +10,16 @@ import org.apache.commons.lang3.StringUtils;
 public class PostQuery extends Query {
 
     private String companyId;
+    private String postName;
 
     @Override
     public void generateCondition() {
+        addSort(Sort.Dir.desc, "postName");
         if (StringUtils.isNotEmpty(companyId)) {
             addFilter(new Filter("company.id", Filter.Operator.eq, companyId));
+        }
+        if (StringUtils.isNotEmpty(postName)) {
+            addFilter(new Filter("postName", Filter.Operator.like, "%" + postName + "%"));
         }
     }
 }
