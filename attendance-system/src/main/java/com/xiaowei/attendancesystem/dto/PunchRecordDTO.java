d7e3884f606eda8d99e1855742ddbc0a1d5539f2
yuanwei <563668429@qq.com>
2018-10-10 17:57:24 +0800

    修改打卡状态

--- attendance-system/src/main/java/com/xiaowei/attendancesystem/dto/PunchRecordDTO.java
+++ attendance-system/src/main/java/com/xiaowei/attendancesystem/dto/PunchRecordDTO.java
@@ -1,6 +1,7 @@
 package com.xiaowei.attendancesystem.dto;
 
 import com.xiaowei.account.entity.SysUser;
+import com.xiaowei.attendancesystem.status.PunchRecordStatus;
 import com.xiaowei.core.validate.V;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -10,7 +11,8 @@ import javax.validation.constraints.NotNull;
 
 @Data
 public class PunchRecordDTO {
-
+    public interface UpdateOnStatus{}
+    public interface UpdateOffStatus{}
     /**
      * 打卡人
      */
@@ -29,5 +31,14 @@ public class PunchRecordDTO {
      */
     @ApiModelProperty(value = "打卡图片")
     private String punchFileStore;
-
+    /**
+     * 上班打卡状态
+     */
+    @NotNull(groups = {PunchRecordDTO.UpdateOnStatus.class},message = "上班打卡状态")
+    private PunchRecordStatus onPunchRecordStatus;
+    /**
+     * 下班打卡状态
+     */
+    @NotNull(groups = {PunchRecordDTO.UpdateOffStatus.class},message = "下班打卡状态")
+    private PunchRecordStatus offPunchRecordStatus;
 }
