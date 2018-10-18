68839170297215cd1d9545fa47ee7b34dda05733
qinyongliang <qinyongliang@hotmail.com>
2018-10-18 17:01:15 +0800

    无奈之举，如果发现是子查询，就溢出掉filter条件

--- account-web/src/main/java/com/xiaowei/accountweb/SqlStatementInspector.java
+++ account-web/src/main/java/com/xiaowei/accountweb/SqlStatementInspector.java
@@ -2,10 +2,16 @@ package com.xiaowei.accountweb;
 
 import org.hibernate.resource.jdbc.spi.StatementInspector;
 
+import java.util.Arrays;
+
 public class SqlStatementInspector implements StatementInspector {
 
     @Override
     public String inspect(String sql) {
+        //无奈之举，如果发现是子查询，就溢出掉filter条件
+        if (Arrays.stream(Thread.getAllStackTraces().get(Thread.currentThread())).limit(20).filter(v -> v.getMethodName().equals("loadCollectionSubselect")).findFirst().isPresent()) {
+            return sql.replaceAll("\\w*\\.tenancy_id=\\? (and|or)?", "");
+        }
         // 这里可以拦截到sql ， 这里的sql格式会有占位符？  如：select u.name from user u where u.id = ?
         return sql;
     }
