b09554f9acd642549ffeb719265696afad8b6893
yuanwei <563668429@qq.com>
2018-10-19 09:56:11 +0800

    tenancyId设置拦截sql

--- account-web/src/main/java/com/xiaowei/accountweb/SqlStatementInspector.java
+++ account-web/src/main/java/com/xiaowei/accountweb/SqlStatementInspector.java
@@ -1,5 +1,9 @@
 package com.xiaowei.accountweb;
 
+import com.xiaowei.accountcommon.LoginUserUtils;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.internal.SessionFactoryImpl;
 import org.hibernate.resource.jdbc.spi.StatementInspector;
 
 import java.util.Arrays;
@@ -8,9 +12,10 @@ public class SqlStatementInspector implements StatementInspector {
 
     @Override
     public String inspect(String sql) {
+
         //无奈之举，如果发现是子查询，就溢出掉filter条件
         if (Arrays.stream(Thread.getAllStackTraces().get(Thread.currentThread())).limit(20).filter(v -> v.getMethodName().equals("loadCollectionSubselect")).findFirst().isPresent()) {
-            return sql.replaceAll("\\w*\\.tenancy_id=\\? (and|or)?", "");
+            return sql.replace("tenancy_id=?", "tenancy_id=\'" + LoginUserUtils.getLoginUser().getTenancyId() + "\'");
         }
         // 这里可以拦截到sql ， 这里的sql格式会有占位符？  如：select u.name from user u where u.id = ?
         return sql;
